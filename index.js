7f21d1f31c5fa24be92c30b281344cad3a646128
Shubham Y <sspy2172002@gmail.com>
2021-08-29 13:24:13 +0530

    fixed some errors in code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 require('dotenv/config');
 
 
-mongoose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true },{ useUnifiedTopology: true },()=>{
+mongoose.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true , useNewUrlParser: true },()=>{
     console.log('connected to db!')
 });
 
